c2944d87795cd1abcabfe417cb8fe1e135da64fd
gr1ph <163816500+gr1ph@users.noreply.github.com>
2024-04-21 16:28:55 -0600

    Create version4.js

---
+++ version4.js
@@ -0,0 +1,211 @@
+let creatures = [];
+let food = [];
+let predators = [];
+let reproductionRate = 0.01;
+let mutationRate = 0.01;
+let populationLimit = 400;
+let limitReached = false;
+let creatureCountInput;
+let foodSlider;
+let predatorCountInput;
+let addPredatorButton;
+let predatorBoundaryCheckbox;
+let boundaryWrapCheckbox;
+
+function setup() {
+  createCanvas(800, 800);
+  for (let i = 0; i < 10; i++) {
+    creatures.push(new Creature(random(width), random(height)));
+  }
+
+  // Create number input field for creatures
+  creatureCountInput = createInput('10', 'number');
+  creatureCountInput.position(10, 10);
+
+  // Create food slider
+  foodSlider = createSlider(0, 100, 50);
+  foodSlider.position(10, 30);
+  foodSlider.style('width', '80px');
+
+  // Create number input field for predators
+  predatorCountInput = createInput('0', 'number');
+  predatorCountInput.position(10, 50);
+
+  // Create button to add predators
+  addPredatorButton = createButton('Add Predator');
+  addPredatorButton.position(140, 50);
+  addPredatorButton.mousePressed(addPredator);
+
+  // Create checkbox for predator boundary wrapping
+  predatorBoundaryCheckbox = createCheckbox('Predator Boundary Wrap', false);
+  predatorBoundaryCheckbox.position(10, 70);
+
+  // Create checkbox for creature and predator boundary wrapping
+  boundaryWrapCheckbox = createCheckbox('Boundary Wrap', false);
+  boundaryWrapCheckbox.position(10, 90);
+}
+
+function draw() {
+  background(220);
+
+  // Update food amount
+  let newFoodAmount = foodSlider.value();
+  if (newFoodAmount < food.length) {
+    food.splice(0, food.length - newFoodAmount);
+  } else {
+    for (let i = food.length; i < newFoodAmount; i++) {
+      food.push(createVector(random(width), random(height)));
+    }
+  }
+
+  // Update predators
+  for (let i = predators.length - 1; i >= 0; i--) {
+    predators[i].update();
+    predators[i].display();
+    if (predators[i].isDead() || (predatorBoundaryCheckbox.checked() && boundaryWrapCheckbox.checked())) {
+      predators.splice(i, 1);
+    }
+  }
+
+  // Update creatures
+  for (let creature of creatures) {
+    creature.update();
+    creature.display();
+
+    // Chance of reproduction
+    if (creatures.length < populationLimit && random(1) < reproductionRate) {
+      let child = creature.reproduce();
+      if (random(1) < mutationRate) {
+        child.mutate();
+      }
+      creatures.push(child);
+    }
+
+    // Check for predator
+    for (let predator of predators) {
+      if (dist(creature.position.x, creature.position.y, predator.position.x, predator.position.y) < creature.size) {
+        creature.die();
+      }
+    }
+  }
+
+  // Check if population limit is reached
+  if (creatures.length >= populationLimit && !limitReached) {
+    limitReached = true;
+    alert("Limit Reached");
+  }
+
+  // Wrap around the boundaries
+  if (boundaryWrapCheckbox.checked()) {
+    for (let creature of creatures) {
+      creature.wrap();
+    }
+    for (let predator of predators) {
+      predator.wrap();
+    }
+  }
+}
+
+function addPredator() {
+  let count = int(predatorCountInput.value());
+  for (let i = 0; i < count; i++) {
+    predators.push(new Predator(random(width), random(height)));
+  }
+}
+
+function mouseClicked() {
+  if (creatureCountInput.value() > 0) {
+    let count = int(creatureCountInput.value());
+    for (let i = 0; i < count; i++) {
+      creatures.push(new Creature(width / 2, height / 2));
+    }
+  }
+}
+class Creature {
+  constructor(x, y) {
+    this.position = createVector(x, y);
+    this.velocity = p5.Vector.random2D();
+    this.velocity.setMag(random(2, 4));
+    this.energy = 100;
+    this.size = 5;
+  }
+
+  update() {
+    // Move
+    this.position.add(this.velocity);
+    this.energy -= 0.1;
+
+    // Eat food
+    for (let i = food.length - 1; i >= 0; i--) {
+      let f = food[i];
+      if (dist(this.position.x, this.position.y, f.x, f.y) < this.size) {
+        this.energy += 20;
+        food.splice(i, 1);
+      }
+    }
+
+    // Check energy level
+    if (this.energy <= 0) {
+      this.die();
+    }
+  }
+
+  display() {
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(this.position.x, this.position.y, this.size * 2, this.size * 2);
+  }
+
+  reproduce() {
+    this.energy /= 2;
+    return new Creature(this.position.x, this.position.y);
+  }
+
+  mutate() {
+    this.velocity.rotate(random(-PI / 4, PI / 4));
+  }
+
+  die() {
+    let index = creatures.indexOf(this);
+    creatures.splice(index, 1);
+  }
+
+  wrap() {
+    if (this.position.x < 0) this.position.x = width;
+    if (this.position.x > width) this.position.x = 0;
+    if (this.position.y < 0) this.position.y = height;
+    if (this.position.y > height) this.position.y = 0;
+  }
+}
+
+class Predator {
+  constructor(x, y) {
+    this.position = createVector(x, y);
+    this.velocity = p5.Vector.random2D();
+    this.velocity.setMag(random(2, 4));
+    this.size = 10;
+    this.lifespan = 1000;
+  }
+
+  update() {
+    this.position.add(this.velocity);
+    this.lifespan--;
+  }
+
+  display() {
+    fill(0, 0, 255);
+    noStroke();
+    ellipse(this.position.x, this.position.y, this.size * 2, this.size * 2);
+  }
+
+  isDead() {
+    return this.lifespan <= 0;
+  }
+
+  wrap() {
+    if (this.position.x < 0) this.position.x = width;
+    if (this.position.x > width) this.position.x = 0;
+    if (this.position.y < 0) this.position.y = height;
+    if (this.position.y > height) this.position.y = 0;
+  }
+}
